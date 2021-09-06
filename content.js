a97ac600c7136d0a5263a2099d78174abb2e5aa2
Pankaj Rathore <65541151+olive-green@users.noreply.github.com>
2021-09-06 04:21:21 -0700

    fix[2]- all bugs fixes

--- content.js
+++ content.js
@@ -1,5 +1,6 @@
+
 const gotUrl=(message,sender,sendResponse)=>{
-    // console.log(message.url)
+    // console.log(message)
     // url=message.url
     if(message.type=="image"){
         var loadImageButton= document.getElementsByClassName("step")[0].lastElementChild;
@@ -9,32 +10,51 @@ const gotUrl=(message,sender,sendResponse)=>{
         let continueButton=document.getElementById("image-mapper-continue");    
         continueButton.disabled=false
         continueButton.click();
+      
     }
-    else {
-        console.log(message.url);
-        
-        //getting image-mapper-table
-        const mapperTableInputs=document.getElementById("image-mapper-table").getElementsByTagName("input");
 
-        // console.log(mapperTableInputs)
-
-        for(var i = 0; i < mapperTableInputs.length; i++){
-            if(mapperTableInputs[i].type === "radio" & mapperTableInputs[i].checked===true){
-                // console.log(mapperTableInputs[i]);
-                console.log(mapperTableInputs[i].parentElement.parentElement.nextSibling.nextSibling.firstChild);
-                mapperTableInputs[i].parentElement.parentElement.nextSibling.nextSibling.firstChild.value=message.url;
-
-            }
-        }
-        
+    else{
+                      
+            $("#downloadButton").attr("download",`${message.fileName}.html`)
+            
     }
     
 }
 chrome.runtime.onMessage.addListener(gotUrl);
 
 
-// const gotCampaingUrl=(message,sender,sendResponse)=>{
-//     console.log(message);
-// }
-// chrome.runtime.onMessage.addListener(gotCampaingUrl);
+
+// crating a download html file button
+var htmlFileButton=`<a class="btn btn-success btn-lg" id="downloadButton" style="margin-left:5px;" download="aster.html">Download</a>`        
+
+//adding download button beside show me code button
+$("div.segment button").after(htmlFileButton);
+
+
+//getting preHeader from local storage
+var header;
+chrome.storage.local.get(['tableRows','preHeader'],function(message){
+    console.log(message.preHeader)
+    // preHeader=message.preHeader;
+    header=`<p style='color: gray; font-family: Verdana, Geneva, sans-serif; font-size: 11px; line-height: 13px; text-align: center; width: 800px'>${message.preHeader}</p>`
+})
+
+$("#downloadButton").click(function(){
+    //when the user clicks on download button then show me code button and its download button gets closed
+    $("div.segment button").click();
+    $(".modal-header  button").click();
+    
+ 
+    const htmlFileData=document.getElementById("modal-code-result").value;
+        // console.log(htmlFileData)
+        // console.log(preHeader)
+        const footer=`<p style='color: gray; font-family: Verdana, Geneva, sans-serif; font-size: 11px; line-height: 13px; text-align: center; width: 800px'> To remove your email address from our mailing list please click on&nbsp;<a href='{{{unsubscribe}}}'>unsubscribe</a></p>`
+        let blob= new Blob([header,htmlFileData,footer],{type:"text/plain;charset=utf-8"})
+        $(this).attr("href",URL.createObjectURL(blob));
+   
+    
+});
+   
+
+
 
--- myscript.js
+++ myscript.js
@@ -1,122 +1,222 @@
-const imgForm=document.getElementById("imgForm");
-const inpFile=document.getElementById("inpFile");
+//function for uploading image into the user page
+imgForm.addEventListener('submit', (e) => {
+	e.preventDefault();
 
-imgForm.addEventListener("submit", e =>{
-    e.preventDefault();
+	//make visible the spinner
+	$('.loading-icon').removeClass('hide');
+	$('#upload-text').text('Uploading..');
 
-    const url="https://api.imgbb.com/1/upload?key=e5b29e6df5494fda797b1bef038a5288";
-    const formData= new FormData();
+	const imgForm = document.getElementById('imgForm');
+	const inpFile = document.getElementById('inpFile');
+	const url =
+		'https://api.imgbb.com/1/upload?key=e5b29e6df5494fda797b1bef038a5288';
+	const formData = new FormData();
 
-    formData.append("image",inpFile.files[0]);
+	const file = inpFile.files[0];
 
-    fetch(url,{
-        method:"post",
-        body:formData
-    }).then(res =>{
-        alert("uploaded successfully")
-        // console.log(res)
-        return res.json();
-    })
-    .then(result=> {
-        console.log(result)
-       // Update the relevant fields with the new data.
-        const sendingUrl = {
-          url:result.data.display_url,
-          type:"image"
-        }
+	console.log(file);
+	if (file) {
+		const reader = new FileReader();
 
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-          chrome.tabs.executeScript(tabs[0].id, {
-              file: "content.js"
-          }, function(){
-              chrome.tabs.sendMessage(tabs[0].id,sendingUrl);
-          });
-      });
+		reader.readAsDataURL(file);
+		reader.onload = function (event) {
+			const imgElement = document.createElement('img');
+			imgElement.src = event.target.result;
+			// document.querySelector("#inputImage").src=event.target.result;
 
-    })
-    .catch(error=>{
-        alert("uploading failed")
-        console.log(error)
-    }) 
-})
+			imgElement.onload = function (e) {
+				// alert(e.target.height + e.target.width)
+				if (e.target.width > 800) {
+					const canvas = document.createElement('canvas');
+					const MAX_WIDTH = 800;
+					const scaleSize = MAX_WIDTH / e.target.width;
+					canvas.width = MAX_WIDTH;
+					canvas.height = e.target.height * scaleSize;
+
+					const ctx = canvas.getContext('2d');
+					ctx.drawImage(e.target, 0, 0, canvas.width, canvas.height);
+					const dataUrl = canvas.toDataURL('image/jpeg');
+
+					const base64img = dataUrl.split(',')[1];
+
+					console.log(base64img);
+
+					formData.append('image', base64img);
+
+					// formData.append("image",dataUrl)
+					// console.log(dataUrl);
+				} else {
+					formData.append('image', inpFile.files[0]);
+				}
+
+				fetch(url, {
+					method: 'post',
+					body: formData,
+				})
+					.then((res) => {
+						// console.log(res)
+						return res.json();
+					})
+					.then((result) => {
+						//hide the spinner
+						$('.loading-icon').addClass('hide');
+						$('#upload-text').text('Upload');
+
+						alert('uploaded successfully');
+						console.log(result);
+						// Update the relevant fields with the new data.
+						const sendingUrl = {
+							url: result.data.image.url,
+							type: 'image',
+						};
+
+						chrome.tabs.query(
+							{ active: true, currentWindow: true },
+							function (tabs) {
+								chrome.tabs.executeScript(
+									tabs[0].id,
+									{
+										file: 'content.js',
+									},
+									function () {
+										chrome.tabs.sendMessage(tabs[0].id, sendingUrl);
+									}
+								);
+							}
+						);
+					})
+					.catch((error) => {
+						alert('uploading failed');
+						console.log(error);
+					});
+			};
+		};
+	}
+});
 
 
-$(document).ready(function(){
-    $("#fileUploader").change(function(evt){
-        let selectedFile=evt.target.files[0];
-        let reader= new FileReader()
-        reader.readAsBinaryString(selectedFile)
-        reader.onload= function(e){
-                    // the files text will be printed here
-                    // console.log(e.target.result)
-                    let data=e.target.result;
-                    let workbook=XLSX.read(data,{
-                        type:"binary"
-                    });
 
-            workbook.SheetNames.forEach(function(sheet,index){
-                if(index==1) return;
-    
-                let XL_row_object=XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheet])
-                console.log(XL_row_object)
-               
+//sending html filename to content.js
+$('#fileName').change(function (event) {
+	const sendingFileName = {
+		fileName: $(this).val(),
+		type: 'fileName',
+	};
 
-                let table= `<table class="table-striped border-success">
-                <thead>
-                    <tr>
-                    <th data-field="id">
-                        <span class="text-success">
-                        Campaign Name
-                        </span>
-                    </th>
-                    <th data-field="name">
-                        <span class="text-success">
-                        Campaign Links 
-                        </span>
-                    </th>
-                    </tr>
-                </thead>
-                `
-             table+="<tbody>";
-            for(let i=10;i<=28;i++){
-                table+=`<tr>
-                            <td>${XL_row_object[i].__EMPTY}</td>
-                            <td><a class="campaign-links" href="${XL_row_object[i]["Aster Online"]}">${XL_row_object[i]["Aster Online"]}<a></td>
-                        </tr>`
-            }               
-            table+=`</tbody>
-                    </table>`
+	chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+		chrome.tabs.executeScript(
+			tabs[0].id,
+			{
+				file: 'content.js',
+			},
+			function () {
+				chrome.tabs.sendMessage(tabs[0].id, sendingFileName);
+				console.log('message sent to conetent.js');
+			}
+		);
+	});
+});
 
-            document.getElementById("linksTable").innerHTML=table;
+//function for changing data into table;
+function getExcelTable() {
+	chrome.storage.local.get(['tableRows', 'preHeader'], function (message) {
+		if (message.tableRows) {
+			console.log(message);
+			// console.log(message.preHeader);
 
+			let table = `<table class="table-striped border-success" id="campaign-table">
+        <thead>
+            <tr>
+            <th data-field="id">
+                <span class="text-success">
+                Campaign Name
+                </span>
+            </th>
+            <th data-field="name">
+                <span class="text-success">
+                Campaign Links 
+                </span>
+            </th>
+            </tr>
+        </thead>
+        `;
+			table += '<tbody>';
+			let XL_row_object = message.tableRows;
 
-            //sending campaign links
-            
-            $('.campaign-links').click(function(){
-                // alert($(this).attr('href'));
-                // or alert($(this).hash();
-                const sendingCampaignLink = {
-                    url:$(this).attr('href'),
-                    type:"table"
-                }
-        
-                chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-                    chrome.tabs.executeScript(tabs[0].id, {
-                        file: "content.js"
-                    }, function(){
-                        chrome.tabs.sendMessage(tabs[0].id,sendingCampaignLink);
-                    });
-                });
-                });   
-            })
-            
+			XL_row_object.forEach(function (row, i) {
+				if (row['__EMPTY']) {
+					// console.log(row["Aster Online"]);
+					table += `<tr>
+                <td>${row['__EMPTY']}</td>
+                <td><a class="campaign-links" href="${row['Aster Online']}">${row['Aster Online']}<a></td>
+            </tr>`;
+				}
+			});
+			table += `</tbody>
+            </table>`;
 
-            reader.onerror= function(event){
-                console.error("File could not be read!"+ event.target.error.code)
-            }
-        }
-        
-    })
-})
+			document.getElementById('linksTable').innerHTML = table;
+		}
+		//copying campaign links on clipboard
+		$('.campaign-links').click(function () {
+			//copy text to clipboard
+			navigator.clipboard.writeText($(this).attr('href'));
+		});
+	});
+}
 
+//reading excel file data and converting it into table form
+$('#fileUploader').change(function (evt) {
+	let selectedFile = evt.target.files[0];
+	let reader = new FileReader();
+	reader.readAsBinaryString(selectedFile);
+	reader.onload = function (e) {
+		// the files text will be printed here
+		// console.log(e.target.result)
+		let data = e.target.result;
+		let workbook = XLSX.read(data, {
+			type: 'binary',
+		});
+
+		workbook.SheetNames.forEach(function (sheet, index) {
+			if (index == 1) return;
+
+			let XL_row_object = XLSX.utils.sheet_to_row_object_array(
+				workbook.Sheets[sheet]
+			);
+			console.log(XL_row_object);
+
+			var preHeader;
+
+			XL_row_object.forEach(function (row, i) {
+				if (row['Campaign Name : '] === 'Pre-header') {
+					// console.log(row["Aster Online"])
+					preHeader = row['Aster Online'];
+				}
+			});
+
+			chrome.storage.local.clear(function () {
+				error = chrome.runtime.lastError;
+				if (error) {
+					alert(error);
+				}
+			});
+			//storing table data locally
+			chrome.storage.local.set(
+				{ tableRows: XL_row_object, preHeader: preHeader },
+				function () {
+					console.log('your data is saved in local storage');
+				}
+			);
+
+			getExcelTable();
+
+			reader.onerror = function (event) {
+				console.error('File could not be read!' + event.target.error.code);
+			};
+		});
+	};
+});
+
+getExcelTable();
 
